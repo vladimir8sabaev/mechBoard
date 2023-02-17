32b2199c1ce8abd5702d0cd8eb95712298381619
Vladimir <vladimir8sabaev@mail.ru>
2023-02-17 18:21:13 +0300

    made bautiful and meaningless chart

--- src/modules/chart.js
+++ src/modules/chart.js
@@ -2,26 +2,49 @@
 import Chart from "chart.js/auto";
 
 async function chart() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
-  new Chart(document.getElementById("acquisitions"), {
+  const data = {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [
+      {
+        label: "Weekly Sales",
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: [
+          "rgba(255, 26, 104, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(0, 0, 0, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 26, 104, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(0, 0, 0, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // config
+  const config = {
     type: "bar",
-    data: {
-      labels: data.map((row) => row.year),
-      datasets: [
-        {
-          label: "Acquisitions by year",
-          data: data.map((row) => row.count),
+    data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
-      ],
+      },
     },
-  });
+  };
+
+  // render init block
+  const myChart = new Chart(document.getElementById("myChart"), config);
 }
 export default chart;
